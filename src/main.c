ca4e1debce84d51e8e4acae41db7a7566932e320
Alvaro Prieto <apg88zx@gmail.com>
2014-01-18 06:30:02 -0800

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx.h"
+#include "main.h"
+
+#include "usbd_cdc_core.h"
+#include "usbd_usr.h"
+#include "usbd_desc.h"
+#include "usbd_cdc_vcp.h"
+
+// Private variables
+volatile uint32_t time_var1, time_var2;
+__ALIGN_BEGIN USB_OTG_CORE_HANDLE  USB_OTG_dev __ALIGN_END;
+
+// Private function prototypes
+void Delay(volatile uint32_t nCount);
+void init();
+void calculation_test();
+
+int main(void) {
+	init();
+
+	/*
+	 * Disable STDOUT buffering. Otherwise nothing will be printed
+	 * before a newline character or when the buffer is flushed.
+	 */
+	setbuf(stdout, NULL);
+
+	calculation_test();
+
+	for(;;) {
+		GPIO_SetBits(GPIOD, GPIO_Pin_12);
+		Delay(500);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+		Delay(500);
+	}
+
+	return 0;
+}
+
+void calculation_test() {
+	float a = 1.001;
+	int iteration = 0;
+
+	for(;;) {
+		GPIO_SetBits(GPIOD, GPIO_Pin_12);
+		Delay(500);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+		Delay(500);
+
+		time_var2 = 0;
+		for (int i = 0;i < 1000000;i++) {
+			a += 0.01 * sqrtf(a);
+		}
+
+		printf("Time:      %lu ms\n\r", time_var2);
+		printf("Iteration: %i\n\r", iteration);
+		printf("Value:     %.5f\n\n\r", a);
+
+		iteration++;
+	}
+}
+
+void init() {
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	// ---------- SysTick timer -------- //
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+		// Capture error
+		while (1){};
+	}
+
+	// ---------- GPIO -------- //
+	// GPIOD Periph clock enable
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	// Configure PD12, PD13, PD14 and PD15 in output pushpull mode
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	// ------------- USB -------------- //
+	USBD_Init(&USB_OTG_dev,
+	            USB_OTG_FS_CORE_ID,
+	            &USR_desc,
+	            &USBD_CDC_cb,
+	            &USR_cb);
+}
+
+/*
+ * Called from systick handler
+ */
+void timing_handler() {
+	if (time_var1) {
+		time_var1--;
+	}
+
+	time_var2++;
+}
+
+/*
+ * Delay a number of systick cycles (1ms)
+ */
+void Delay(volatile uint32_t nCount) {
+	time_var1 = nCount;
+	while(time_var1){};
+}
+
+/*
+ * Dummy function to avoid compiler error
+ */
+void _init() {
+
+}
+
