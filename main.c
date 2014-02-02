1929bccfbfb89c0a1a974d7ff64cbcdde610674d
Alvaro Prieto <apg88zx@gmail.com>
2014-02-02 12:57:04 -0800

    Init cleanup

--- main.c
+++ main.c
@@ -51,7 +51,6 @@ int main(void) {
 }
 
 void init() {
-	GPIO_InitTypeDef  GPIO_InitStructure;
 
 	// ---------- SysTick timer -------- //
 	if (SysTick_Config(SystemCoreClock / 1000)) {
@@ -63,13 +62,7 @@ void init() {
 	// GPIOD Periph clock enable
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	// Configure PD12, PD13, PD14 and PD15 in output pushpull mode
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_Init(GPIOD, &(GPIO_InitTypeDef){GPIO_Pin_12, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_Speed_2MHz, GPIO_PuPd_NOPULL});
 
 	USBD_Init(&USB_OTG_dev,
 				USB_OTG_FS_CORE_ID,
@@ -82,11 +75,3 @@ void SysTick_Handler(void)
 {
 	tickMs++;
 }
-
-/*
- * Dummy function to avoid compiler error
- */
-void _init() {
-
-}
-
